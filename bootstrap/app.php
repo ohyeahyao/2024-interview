8a5f4f5b7c5c629c5a4418aa10f9ed2f41b25b49
yaocase@gmail.com <yaocase@gmail.com>
2024-08-03 15:01:22 +1000

    feat: api routes infrastructure and remove web routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        using: static function(): void
+        {
+            Route::middleware('api')
+            ->prefix('api')
+            ->group(base_path('routes/api.php'));
+        },
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(static function (Middleware $middleware): void
+    {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(static function (Exceptions $exceptions): void
+    {
         //
     })->create();
